560ab4294f0f9f05970d82593ecff0c2b535d87b
Oscar Sorbara <oscarsorbara@MacBook-Air-de-Oscar.local>
2026-02-24 17:20:05 -0300

    Updates

--- assets/checkout-C1id-wvG.js
+++ assets/checkout-C1id-wvG.js
@@ -1,4 +1,4 @@
-import{s as u}from"./index-Cdhj8SbL.js";const h=`
+import{s as u}from"./index-B2aZdbC4.js";const h=`
   mutation cartCreate($lines: [CartLineInput!]!, $discountCodes: [String!]) {
     cartCreate(input: { lines: $lines, discountCodes: $discountCodes }) {
       cart {
