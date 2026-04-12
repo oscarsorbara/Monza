821759765cff218d04d4d0819382bbaf58a2f382
Oscar Sorbara <oscarsorbara@MacBook-Air-de-Oscar.local>
2026-04-11 22:14:27 -0300

    Updates

--- assets/checkout-CMwBjdP8.js
+++ assets/checkout-CMwBjdP8.js
@@ -1,4 +1,4 @@
-import{s as u}from"./index-BJb8dKeD.js";const h=`
+import{s as u}from"./index-kL0oTmMG.js";const h=`
   mutation cartCreate($lines: [CartLineInput!]!, $discountCodes: [String!]) {
     cartCreate(input: { lines: $lines, discountCodes: $discountCodes }) {
       cart {
