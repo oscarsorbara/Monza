efddb2b82b7b26e8a6c4a7e74ea153843f4149d3
Oscar Sorbara <oscarsorbara@MacBook-Air-de-Oscar.local>
2026-04-14 17:50:48 -0300

    Updates

--- assets/checkout-xkDymLye.js
+++ assets/checkout-xkDymLye.js
@@ -1,4 +1,4 @@
-import{s as u}from"./index-BxTC46xB.js";const h=`
+import{s as u}from"./index-BSTBcEqs.js";const h=`
   mutation cartCreate($lines: [CartLineInput!]!, $discountCodes: [String!]) {
     cartCreate(input: { lines: $lines, discountCodes: $discountCodes }) {
       cart {
