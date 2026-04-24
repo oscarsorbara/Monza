8f627c169494831ba831984f46b723bbbc067596
Oscar Sorbara <oscarsorbara@MacBook-Air-de-Oscar.local>
2026-04-24 09:52:47 -0300

    Updates

--- assets/checkout-Bxp-YkKa.js
+++ assets/checkout-Bxp-YkKa.js
@@ -1,4 +1,4 @@
-import{s as u}from"./index-BhySsCYc.js";const h=`
+import{s as u}from"./index-DRFh6pHh.js";const h=`
   mutation cartCreate($lines: [CartLineInput!]!, $discountCodes: [String!]) {
     cartCreate(input: { lines: $lines, discountCodes: $discountCodes }) {
       cart {
