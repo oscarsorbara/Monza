41430e5a4e6e283bc934851e014badeae890c164
Oscar Sorbara <oscarsorbara@MacBook-Air-de-Oscar.local>
2026-04-27 11:23:15 -0300

    Updates

--- assets/checkout-Davn0-ET.js
+++ assets/checkout-Davn0-ET.js
@@ -1,4 +1,4 @@
-import{s as u}from"./index-v_sOV-dG.js";const h=`
+import{s as u}from"./index-C-_MCWVx.js";const h=`
   mutation cartCreate($lines: [CartLineInput!]!, $discountCodes: [String!]) {
     cartCreate(input: { lines: $lines, discountCodes: $discountCodes }) {
       cart {
