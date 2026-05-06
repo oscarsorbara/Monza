7c76d25c328fea70b49349b036b145e2ccf0abd9
Oscar Sorbara <oscarsorbara@MacBook-Air-de-Oscar.local>
2026-05-06 15:46:43 -0300

    Updates

--- assets/checkout-gwAUv7OS.js
+++ assets/checkout-gwAUv7OS.js
@@ -1,4 +1,4 @@
-import{s as u}from"./index-17e7omE8.js";const h=`
+import{s as u}from"./index-CPG6I3zl.js";const h=`
   mutation cartCreate($lines: [CartLineInput!]!, $discountCodes: [String!]) {
     cartCreate(input: { lines: $lines, discountCodes: $discountCodes }) {
       cart {
